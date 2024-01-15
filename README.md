e20aafd9b1ebe98bc23de8d487e32cf13d92131c
Skandori <lucas.jalras@orange.fr>
2024-01-15 11:50:14 +0100

    Mise a disposition du code - 15/01/24 - 11h50

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # LucasVersionduCodeEval15-01-2024
+
+Nom : JALRAS
+Prénom : Lucas
+Version : 1.0.0
+Date : 15/01/2024
---
+++ test/test_Steve.py
@@ -0,0 +1,15 @@
+from Steve import *
+import pytest
+
+def test_decomposer():
+    assert decomposer(254) == (2, 5, 4)
+    assert decomposer(218) == (2, 1, 8)
+
+def test_divisible():
+    assert divisible(81) == True
+    assert divisible(31) == False
+
+def test_somme():
+    assert somme(5, 2, 3) == True
+    assert somme(9, 4, 5) == True
+    assert somme(5, 5, 4) == False
